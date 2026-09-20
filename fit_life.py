20edc2ba79dff1536c4f14a756849eda330c7fc0
Матвей Хрестьянович <qumikk@yandex.ru>
2026-09-21 01:06:42 +0600

    Закончил

--- fit_life.py
+++ fit_life.py
@@ -1,13 +1,9 @@
 import sys
 
-
-
-
 sys.stdout.reconfigure(encoding='utf-8')
 # step 1
 user_name = input('Приветствую тебя, как к тебе можно обращаться?')
 user_age = int(input('Сколько вам полных лет?'))
-
 
 # step 2
 user_weight = float(input('Какой у Вас вес (в кг)?'))
@@ -17,7 +13,6 @@ user_height = float(input('Какой у Вас рост (в метрах)?'))
 bmi = round(user_weight / (user_height ** 2), 1)
 water_needed_ml = user_weight * 30
 water_needed_l = round(water_needed_ml / 1000, 1)
-
 
 # Step 4
 print(f'Приятно познакомиться {user_name}, Ваш возраст {user_age}')
